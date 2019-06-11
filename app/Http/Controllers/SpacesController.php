adcdd00b7e61d63ccc356a5e17a993eacaa7b934
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-11 15:00:57 +0100

    return view on default space view

--- app/Http/Controllers/SpacesController.php
+++ app/Http/Controllers/SpacesController.php
@@ -3,8 +3,22 @@
 namespace Convene\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
+/**
+ * Class SpacesController.
+ *
+ * @package Convene\Http\Controllers
+ */
 class SpacesController extends Controller
 {
-    //
+    /**
+     * Show the spaces list view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index(): View
+    {
+        return view('spaces');
+    }
 }
