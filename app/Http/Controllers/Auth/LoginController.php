9e7b458656cbdc441b479c010f7ab83553e35f03
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-11 11:59:48 +0100

    working on user authentication. #1

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/default';
 
     /**
      * Create a new controller instance.
