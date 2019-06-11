26a89300101e436bedb634eb3f862d26bf00473a
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-11 13:56:59 +0100

    added User Role to Storage Provider

--- app/Providers/StorageProvider.php
+++ app/Providers/StorageProvider.php
@@ -21,8 +21,12 @@ class StorageProvider extends ServiceProvider
      */
     protected $aliases = [
         'user.entity'       => [\Convene\Storage\Entity\Contract\UserEntityInterface::class],
-         'user.repository'   => [\Convene\Storage\Repository\Contract\UserRepositoryInterface::class],
-         'user.service'      => [\Convene\Storage\Service\Contract\UserServiceInterface::class],
+        'user.repository'   => [\Convene\Storage\Repository\Contract\UserRepositoryInterface::class],
+        'user.service'      => [\Convene\Storage\Service\Contract\UserServiceInterface::class],
+
+        'userRole.entity'  => [\Convene\Storage\Entity\Contract\UserRoleEntityInterface::class],
+        'userRole.repository' => [\Convene\Storage\Repository\Contract\UserRoleRepositoryInterface::class],
+        'userRole.service' => [\Convene\Storage\Service\Contract\UserRoleServiceInterface::class],
     ];
 
     /**
@@ -46,6 +50,7 @@ class StorageProvider extends ServiceProvider
     protected function registerEntities(): void
     {
         $this->app->bind('user.entity', \Convene\Storage\Entity\UserEntity::class);
+        $this->app->bind('userRole.entity', \Convene\Storage\Entity\UserRoleEntity::class);
     }
 
     /**
@@ -56,6 +61,7 @@ class StorageProvider extends ServiceProvider
     protected function registerRepositories(): void
     {
          $this->app->singleton('user.repository', \Convene\Storage\Repository\UserRepository::class);
+         $this->app->singleton('userRole.repository', \Convene\Storage\Repository\UserRoleRepository::class);
     }
 
     /**
@@ -66,5 +72,6 @@ class StorageProvider extends ServiceProvider
     protected function registerServices(): void
     {
          $this->app->singleton('user.service', \Convene\Storage\Service\UserService::class);
+         $this->app->singleton('userRole.service', \Convene\Storage\Service\UserRoleService::class);
     }
 }
