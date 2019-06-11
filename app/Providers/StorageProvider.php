cc54047613a1e59e704923b01cbae9dfec62e23b
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-11 11:59:06 +0100

    started provider to add storage access to application

---
+++ app/Providers/StorageProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Convene\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class StorageProvider.
+ *
+ * @package Convene\Providers
+ */
+class StorageProvider extends ServiceProvider
+{
+
+}
