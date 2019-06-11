dc0d26eccb462939e04d87c4c6eacadfe51b3810
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-11 15:26:17 +0100

    added page title buttons option to header

--- resources/views/layouts/components/_page-title.blade.php
+++ resources/views/layouts/components/_page-title.blade.php
@@ -1,10 +1,14 @@
 <!-- Page Heading -->
-<h3 class="h1 mb-0 text-gray-800">
-    @foreach((array) $title as $step)
-        @if(! $loop->last )
-            <span class="text-secondary d-inline-block">{{ $step }} &rsaquo; </span>
-        @else
-            <span class="text-dark inline-block">{{ $step }}</span>
-        @endif
-    @endforeach
-</h3>
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h2 mb-0 text-gray-800">
+        @foreach((array) $title as $step)
+            @if(! $loop->last )
+                <span class="text-secondary d-inline-block">{{ $step }} &rsaquo; </span>
+            @else
+                <span class="text-dark inline-block">{{ $step }}</span>
+            @endif
+        @endforeach
+    </h1>
+
+    @stack('title_buttons')
+</div>
