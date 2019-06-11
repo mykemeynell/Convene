0ab52ff39b8e32af75860c5f631380138abc8b4b
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-11 15:28:16 +0100

    added new space button to header on space view

--- resources/views/spaces.blade.php
+++ resources/views/spaces.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.page', ['page_title' => 'Spaces'])
 
+@push('title_buttons')
+    <button class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#new-space-modal">Create Space</button>
+@endpush
+
 @section('body')
 
     <div class="row">
