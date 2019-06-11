74a0dbf03abc808932284e64cf55870fe00e3a44
Myke Meynell <myke@wearebump.com>
2019-06-11 19:51:26 +0100

    created interface for spaces repository

---
+++ app/Storage/Repository/Contract/SpacesRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Convene\Storage\Repository\Contract;
+
+use ArchLayer\Repository\RepositoryInterface;
+
+/**
+ * Interface SpacesRepositoryInterface
+ *
+ * @package Convene\Storage\Repository\Contract
+ */
+interface SpacesRepositoryInterface extends RepositoryInterface
+{
+    //
+}
