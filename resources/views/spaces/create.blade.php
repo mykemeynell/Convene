d37223ef86bd69c807f2a622bd24ec6d42d0bbee
Myke Meynell <myke@wearebump.com>
2019-06-12 10:51:09 +0100

    started working on form for creating a new space

--- resources/views/spaces/create.blade.php
+++ resources/views/spaces/create.blade.php
@@ -12,32 +12,20 @@
 @section('body')
 
     <div class="row">
-        <div class="col-12">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>Space</th>
-                        <th>Description</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>
-                            <img src="https://placehold.it/50" alt="Space Icon">
-                            <span>Space name</span>
-                        </td>
-                        <td>
-                            Description of space
-                        </td>
-                        <td align="right">
-                            <a href="#" class="btn btn-outline-dark btn-sm">Options</a>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
+        <div class="col-6 offset-3">
+
+            <form action="">
+                <div class="form-group">
+                    <label for="space-name">Name:</label>
+                    <input name="space[name]" id="space-name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="space-description">Description:</label>
+                    <textarea name="space[description]" id="space-description" class="form-control"></textarea>
+                </div>
+            </form>
+
         </div>
     </div>
 
