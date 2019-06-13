f5376f27bac44812594eeab2ae12062602f45724
Myke Meynell <myke@wearebump.com>
2019-06-13 19:43:57 +0100

    Created HasService trait, can be used when adding a (or multiple) services to a parent object

---
+++ app/Support/Concern/HasService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Convene\Support\Concern;
+
+use ArchLayer\Service\Contract\ServiceInterface;
+use Illuminate\Support\Collection;
+
+/**
+ * Trait HasService
+ *
+ * @package Convene\Support\Concern
+ */
+trait HasService {
+
+    /**
+     * Holds the services that are used within the parent object.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    protected $services;
+
+    /**
+     * Set a service within the object $services property.
+     *
+     * @param \ArchLayer\Service\Contract\ServiceInterface $service
+     * @param string|null                                  $name
+     *
+     * @return Collection
+     */
+    protected function setService(ServiceInterface $service, ?string $name = null): Collection
+    {
+        return $this->services->put(self::normalizeName($name), $service);
+    }
+
+    /**
+     * Get a service that has been added into the parent object. A name can be specified if more than one
+     * service has been registered.
+     *
+     * @param string|null $name
+     *
+     * @return \ArchLayer\Service\Contract\ServiceInterface
+     */
+    protected function getService(?string $name = null): ServiceInterface
+    {
+        return $this->services->get(self::normalizeName($name));
+    }
+
+    /**
+     * Return a normalized string when getting or setting service by name.
+     * Ensures that when a service is set, the string does not contain characters
+     * that could cause unexpected or unwanted behaviour.
+     *
+     * @param string|null $name
+     *
+     * @return string
+     */
+    private static function normalizeName(?string $name = null): string
+    {
+        return ! empty($name)
+            ? normalizer_normalize($name)
+            : 'default';
+    }
+}
