3ca25bb4b1b4fa7a23d5cb7462954a861191022e
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-15 17:00:35 +0100

    upaded space navigation items

--- resources/views/layouts/components/_space-items.blade.php
+++ resources/views/layouts/components/_space-items.blade.php
@@ -12,21 +12,10 @@
         <span>Activity</span></a>
 </li>
 
+@foreach($space->items() as $s_page)
 <li class="nav-item">
-    <a class="nav-link" href="#">
+    <a class="nav-link" href="{{ route('page.showSpace', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()]) }}">
         <i class="fas fa-fw fa-file"></i>
-        <span>Welcome to Convene</span></a>
+        <span>{{ $s_page->getDisplayName() }}</span></a>
 </li>
-
-<li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-        <i class="fas fa-fw fa-folder"></i>
-        <span>General Help</span>
-    </a>
-    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar" style="">
-        <div class="bg-white py-2 collapse-inner rounded">
-            <a class="collapse-item" href="#">WiFi Passwords</a>
-            <a class="collapse-item" href="#">Creating a WiFi network</a>
-        </div>
-    </div>
-</li>
+@endforeach
