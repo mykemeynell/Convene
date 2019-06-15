1d65c1df537034913afef483116e497a473370a1
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-15 16:53:10 +0100

    added additional routes to show space sidebar

--- resources/views/layouts/components/_sidebar.blade.php
+++ resources/views/layouts/components/_sidebar.blade.php
@@ -25,7 +25,7 @@
             <span>Spaces</span></a>
     </li>
 
-    @if(in_array($route, ['spaces.showSpace', 'spaces.showActivity', 'page.showSpace']))
+    @if(in_array($route, ['spaces.showSpace', 'spaces.showActivity', 'page.showSpace', 'page.showCreate']))
         @include('layouts.components._space-items')
     @endif
 
