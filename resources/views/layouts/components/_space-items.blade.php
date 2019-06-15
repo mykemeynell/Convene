ef9ed44a8efaf8610f41b13faea14cc7f3baa831
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-15 22:08:25 +0100

    added edit marker to sidebar navigation and added hint.css for tooltips

--- resources/views/layouts/components/_space-items.blade.php
+++ resources/views/layouts/components/_space-items.blade.php
@@ -14,9 +14,12 @@
 
 
 @foreach($space->items() as $s_page)
-<li class="nav-item @if(url($path) == route('page.showSpace', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()])) active @endif">
+<li class="nav-item @if(
+        url($path) == route('page.showSpace', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()]) ||
+        url($path) == route('page.showEdit', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()])
+    ) active @endif">
     <a class="nav-link" href="{{ route('page.showSpace', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()]) }}">
         <i class="fas fa-fw fa-file"></i>
-        <span>{{ $s_page->getDisplayName() }}</span></a>
+        <span>{{ $s_page->getDisplayName() }}</span>@if(url($path) == route('page.showEdit', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()]))<span class="badge badge-pill badge-light text-primary ml-2 hint--right hint--rounded hint--bounce" aria-label="You are currently editing this item"><i class="text-primary fas fa-pen mr-0"></i></span>@endif</a>
 </li>
 @endforeach
