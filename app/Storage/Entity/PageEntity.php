4d5d154849f6407eb2ebe79efe65c30709fd6615
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-15 13:26:15 +0100

    Created PageEntity

---
+++ app/Storage/Entity/PageEntity.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Convene\Storage\Entity;
+
+use Convene\Storage\Entity\Concern\SlugOptions as SlugOptionsConcern;
+use Convene\Storage\Entity\Contract\PageEntityInterface;
+use Convene\Storage\Entity\Contract\SpaceEntityInterface;
+use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use UuidColumn\Concern\HasUuidObserver;
+
+/**
+ * Class PageEntity
+ *
+ * @package Convene\Storage\Entity
+ */
+class PageEntity extends Model implements PageEntityInterface
+{
+    use HasUuidObserver, HasTimestamps, HasSlug, SlugOptionsConcern;
+
+    /**
+     * Field to generate the slug from.
+     *
+     * @var string
+     */
+    public $createSlugFrom = 'title';
+
+    /**
+     * The table name.
+     *
+     * @var string
+     */
+    protected $table = 'pages';
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['space_id', 'folder_id', 'title', 'slug', 'content',];
+
+    /**
+     * Get the parent space ID.
+     *
+     * @return string
+     */
+    public function getSpaceId(): string
+    {
+        return $this->space_id;
+    }
+
+    /**
+     * Get the folder ID.
+     *
+     * @return string|null
+     */
+    public function getFolderId(): ?string
+    {
+        return $this->folder_id;
+    }
+
+    /**
+     * Get the space.
+     *
+     * @return \Convene\Storage\Entity\Contract\SpaceEntityInterface|null
+     */
+    public function space(): ?SpaceEntityInterface
+    {
+        // TODO: Implement space() method.
+    }
+
+    /**
+     * Get the folder that the page belongs to.
+     *
+     * @return mixed
+     */
+    public function folder()
+    {
+        // TODO: Implement folder() method.
+    }
+
+    /**
+     * Get the display name.
+     * Alias of getTitle().
+     *
+     * @see \Convene\Storage\Entity\Contract\PageEntityInterface::getTitle()
+     *
+     * @return string
+     */
+    public function getDisplayName(): string
+    {
+        return $this->getTitle();
+    }
+
+    /**
+     * Get the title.
+     *
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * Get the slug.
+     *
+     * @return string
+     */
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Get the content as a string.
+     *
+     * @return string|null
+     */
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+}
