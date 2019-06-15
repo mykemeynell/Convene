ac1cf4e55a62e29ab9e26e002b15ba4322ce1362
Myke Meynell <myke@wearebump.com>
2019-06-16 00:19:27 +0100

    #11 added scaffolding for menu item in sidebar

--- resources/views/layouts/components/_space-items.blade.php
+++ resources/views/layouts/components/_space-items.blade.php
@@ -20,6 +20,17 @@
     ) active @endif">
     <a class="nav-link" href="{{ route('page.showSpace', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()]) }}">
         <i class="fas fa-fw fa-file"></i>
-        <span>{{ $s_page->getDisplayName() }}</span>@if(url($path) == route('page.showEdit', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()]))<span class="badge badge-pill badge-light text-primary ml-2 hint--right hint--rounded hint--bounce" aria-label="You are currently editing this item"><i class="text-primary fas fa-pen mr-0"></i></span>@endif</a>
+        <span>{{ $s_page->getDisplayName() }}</span>@if(url($path) == route('page.showEdit', ['space_slug' => $space->getSlug(), 'page_slug' => $s_page->getSlug()]))<span class="badge badge-pill badge-light text-primary ml-2 hint--right hint--rounded hint--bounce" aria-label="You are currently editing this item"><i class="text-primary fas fa-pen mr-0"></i></span>@endif
+    </a>
+    @if($route !== 'page.showEdit')
+    <div class="dropdown space-menu-item-dropdown">
+        <a data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">Move</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item text-danger" href="#">Delete</a>
+        </div>
+    </div>
+    @endif
 </li>
 @endforeach
